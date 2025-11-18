4c45c55991146cc2ff7c3a8b2c14aca92ddfa048
Henry Ha <henry@aiple.co>
2025-11-18 17:49:22 +0900

    fix: fix a bug

--- src/tools/config.js
+++ src/tools/config.js
@@ -1,20 +1,20 @@
-// import {dirname, join} from 'node:path';
-// import {fileURLToPath} from 'node:url';
+import {dirname, join} from 'node:path';
+import {fileURLToPath} from 'node:url';
 import {readFileSync} from 'node:fs';
 
-// import {SSMClient, GetParametersCommand} from '@aws-sdk/client-ssm';
+import {SSMClient, GetParametersCommand} from '@aws-sdk/client-ssm';
 import yargs from 'yargs/yargs';
 
 // assume this is used as local module under node_modules
-// const root = (() => {
-//   const p = fileURLToPath(import.meta.url);
-//   if (p.indexOf('node_modules') > 0) {
-//     return p.split('node_modules')[0];
-//   } else {
-//     // directly used; for debugging
-//     return dirname(join(p, '..', '..'));
-//   }
-// })();
+const root = (() => {
+  const p = fileURLToPath(import.meta.url);
+  if (p.indexOf('node_modules') > 0) {
+    return p.split('node_modules')[0];
+  } else {
+    // directly used; for debugging
+    return dirname(join(p, '..', '..'));
+  }
+})();
 
 async function config() {
   const {argv} = yargs(process.argv.slice(2))
@@ -26,30 +26,28 @@ async function config() {
       }
     });
 
-  return argv;
+  if (argv.ssm?.constructor === Object) {
+    try {
+      const configPath = join(root, 'config.json');
+      console.log(`loading config from ${configPath}`);
+      const fileConfig = JSON.parse(readFileSync(configPath, 'utf8'));
 
-  // if (argv.ssm?.constructor === Object) {
-  //   try {
-  //     const configPath = join(root, 'config.json');
-  //     console.log(`loading config from ${configPath}`);
-  //     const fileConfig = JSON.parse(readFileSync(configPath, 'utf8'));
+      if (!fileConfig.ssm) return fileConfig;
 
-  //     if (!fileConfig.ssm) return fileConfig;
+      const client = new SSMClient(fileConfig.ssm.config);
+      const ssmConfig = await client.send(new GetParametersCommand({
+        Names: [fileConfig.ssm.name],
+        WithDecryption: true,
+      }));
 
-  //     const client = new SSMClient(fileConfig.ssm.config);
-  //     const ssmConfig = await client.send(new GetParametersCommand({
-  //       Names: [fileConfig.ssm.name],
-  //       WithDecryption: true,
-  //     }));
-
-  //     return JSON.parse(ssmConfig.Parameters[0].Value);
-  //   } catch (err) {
-  //     console.log(`failed to load from ssm: ${err}`);
-  //     throw err;
-  //   }
-  // } else {
-  //   return argv;
-  // }
+      return JSON.parse(ssmConfig.Parameters[0].Value);
+    } catch (err) {
+      console.log(`failed to load from ssm: ${err}`);
+      throw err;
+    }
+  } else {
+    return argv;
+  }
 }
 
 export default await config();
