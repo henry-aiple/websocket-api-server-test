7766e7fe10097c68248f61c23d63f1f4d6652d17
Henry Ha <henry@aiple.co>
2025-11-18 17:52:03 +0900

    fix: fix a bug

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,6 +1,7 @@
 import logger from '../tools/logger.js';
 import rdb from './rdb/index.js';
 import documentdb from './documentdb/index.js';
+import dynamodb from './aws/dynamodb/index.js';
 
 export const init = async () => {
   // init code
@@ -12,6 +13,7 @@ export const cleanUp = async () => {
     await rdb.pool.end();
     await rdb.poolRo.end();
     documentdb.closeMongoDbConnection();
+    dynamodb.closeDynamoDBConnection();
   } catch (err) {
     logger.info(`backend cleanUp error. ${err}`);
   }
