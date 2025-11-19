2e787cb3a6bc44e7f4837afee4adc36a75dee484
Henry Ha <henry@aiple.co>
2025-11-19 10:55:07 +0900

    feat(auth): add token query

--- src/controllers/v1/ws/handler.js
+++ src/controllers/v1/ws/handler.js
@@ -1,24 +1,43 @@
-import wsConnectionModel from '../../../../models/wsConnection.js';
-import {v4 as uuidv4} from 'uuid';
+// import wsConnectionModel from '../../../../models/wsConnection.js';
+// import {v4 as uuidv4} from 'uuid';
+import logger from '../../../tools/logger.js';
 
 // This handler will be called when a client connects to the WebSocket
 export const connect = async (req, res) => {
   try {
-    const connectionId = req.headers['sec-websocket-key'];
-    const userId = Math.floor(Math.random() * 1000000); // for example, 0 - 999999
-    const userUid = uuidv4(); // Generate a random UUID
+    logger.info(`req.query: ${JSON.stringify(req.query)}`);
+    logger.info(`req.headers: ${JSON.stringify(req.headers)}`);
 
-    await wsConnectionModel.addItem({
-      connId: connectionId,
-      userId,
-      userUid,
-    });
+    const {token} = req.query;
+    if (!token) {
+      return res.status(401).json({
+        error: 'Token is required',
+      });
+    }
+
+    if (token !== 'xinobi' && token !== 'aiple') {
+      return res.status(401).json({
+        error: 'Invalid token',
+      });
+    }
+
+    const connectionId = req.headers['sec-websocket-key'];
+    console.log(`connectionId: ${connectionId}`);
+
+    // const userId = Math.floor(Math.random() * 1000000); // for example, 0 - 999999
+    // const userUid = uuidv4(); // Generate a random UUID
+
+    // await wsConnectionModel.addItem({
+    //   connId: connectionId,
+    //   userId,
+    //   userUid,
+    // });
 
     return res.status(200).json({
       result: 'success',
       message: 'WebSocket connected',
       connectionId,
-      userUid,
+      // userUid,
     });
   } catch (error) {
     res.status(500).json({
@@ -31,7 +50,7 @@ export const connect = async (req, res) => {
 export const disconnect = async (req, res) => {
   try {
     const connectionId = req.headers['sec-websocket-key'];
-    await wsConnectionModel.deleteByConnId(connectionId);
+    // await wsConnectionModel.deleteByConnId(connectionId);
     return res.status(200).json({
       result: 'success',
       message: 'WebSocket disconnected',
@@ -49,7 +68,9 @@ export const disconnect = async (req, res) => {
 export const sendMessage = async (req, res) => {
   try {
     // Example: echoing the received message
+    logger.info(`req.body: ${JSON.stringify(req.body)}`);
     const message = req.body && req.body.message ? req.body.message : 'No message provided';
+    logger.info(`message: ${message}`);
     return res.status(200).json({
       result: 'success',
       message: 'Message received',
--- src/routes/v1/ws/handler.js
+++ src/routes/v1/ws/handler.js
@@ -1,13 +1,13 @@
 import express from 'express';
 const router = express.Router();
 import wsHandlerController from '../../../controllers/v1/ws/handler.js';
-import auth from '../../../middlewares/auth.js';
+// import auth from '../../../middlewares/auth.js';
 // import validator from '../../../middlewares/validators/index.js';
 
-router.use(auth.authenticateWithBasic);
+// router.use(auth.authenticateWithBasic);
 
 router.route('/connect')
-  .post(wsHandlerController.connect);
+  .get(wsHandlerController.connect);
 
 router.route('/disconnect')
   .post(wsHandlerController.disconnect);
